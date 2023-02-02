5653d3b3375cb3c1d97353ca23188b153cd827fa
Jasper Li <95453968+jasper0730@users.noreply.github.com>
2023-02-03 00:46:17 +0800

    2023.02.03

--- js/main.js
+++ js/main.js
@@ -1,26 +1,26 @@
-const plugin = {};
+// const plugin = {};
 
-plugin.getDataFromInput = function () {
-  const input = document.querySelector("#imgInp");
-  function getData(input) {
-    // 判斷查看有沒有檔案
-    if (input.files && input.files[0]) {
-      // 讀取檔案
-      let reader = new FileReader();
-      reader.onload = function (e) {
-        const previewImg = document.querySelector("#preview_img");
-        previewImg.setAttribute("src", e.target.result);
-      };
-      reader.readAsDataURL(input.files[0]);
-    }
-  }
+// plugin.getDataFromInput = function () {
+//   const input = document.querySelector("#imgInp");
+//   function getData(input) {
+//     // 判斷查看有沒有檔案
+//     if (input.files && input.files[0]) {
+//       // 讀取檔案
+//       let reader = new FileReader();
+//       reader.onload = function (e) {
+//         const previewImg = document.querySelector("#preview_img");
+//         previewImg.setAttribute("src", e.target.result);
+//       };
+//       reader.readAsDataURL(input.files[0]);
+//     }
+//   }
 
-  input.addEventListener("change", function () {
-    console.log(this.parentNode );
-    getData(this);
-  });
-};
+//   input.addEventListener("change", function () {
+//     console.log(this.parentNode );
+//     getData(this);
+//   });
+// };
 
 
-plugin.getDataFromInput();
+// plugin.getDataFromInput();
 
